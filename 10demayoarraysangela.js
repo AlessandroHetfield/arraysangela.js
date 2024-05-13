5a602ba7f0722dc3ddacb0e9aab99dfb78a0e914
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-05-12 21:46:32 -0400

    Add files via upload

---
+++ 10demayoarraysangela.js
@@ -0,0 +1,4 @@
+var guestList = ["Angela", "Jack", 'Pam', "Jame0s", "Laura", 'Jason'];
+//var guestName = prompt("What is your name?");
+//var nombre = prompt('¿Cómo te llamas? '); prompt() es una función de JavaScript estándar para interactuar con el usuario en un navegador. 
+guestList.includes(guestName);
